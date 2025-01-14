a7fc5684676a4ab6f1c5dd8f3f8e57952f66d446
umumararungu <umcynth@gmail.com>
2025-01-14 13:25:58 +0200

    caching

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 0. Basic dictionary
+"""
+
+
+class BaseCaching:
+    'print(__import__("my_module").MyClass.__doc__)'
+    def __init__(self):
+        'print(__import__("my_module").my_function.__doc__)'
+        self.cache_data = {}
+
+class BasicCache(BaseCaching):
+    'print(__import__("my_module").MyClass.__doc__)'
+    def put(self, key, item):
+        'print(__import__("my_module").my_function.__doc__)'
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        'print(__import__("my_module").my_function.__doc__)'
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+    
