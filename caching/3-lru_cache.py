a8b84b094c6323fa1d1c289fd6ad87c61070dbb7
umumararungu <umcynth@gmail.com>
2025-01-16 14:31:55 +0200

    remove

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -21,11 +21,17 @@ class LRUCache(BaseCaching):
         if key is None or item is None:
             return
 
-
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            old_key = self.order.pop()
-            del self.cache_data[old_key]
-            print(f"DISCARD: {old_key}")
+        # Check if the item is already in the cache
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            # Update the order of keys since it's recently used
+            self.order.remove(key)
+            self.order.append(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                old_key = self.order.pop()
+                del self.cache_data[old_key]
+                print(f"DISCARD: {old_key}")
 
         self.cache_data[key] = item
         self.order.append(key)
@@ -35,4 +41,9 @@ class LRUCache(BaseCaching):
         """
         if key is None or key not in self.cache_data:
             return None
+        
+                # Move the accessed key to the end to show that it was recently used
+        self.order.remove(key)
+        self.order.append(key)
+        
         return self.cache_data[key]
