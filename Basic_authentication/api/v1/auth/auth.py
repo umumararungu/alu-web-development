db6870e21e180ca73fc07dc32adb6a6ca83421d5
umumararungu <umcynth@gmail.com>
2025-01-22 13:35:24 +0200

    lines

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -11,11 +11,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''require auth function'''
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         '''authorization header function'''
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         '''current user fucntion'''
         return None
