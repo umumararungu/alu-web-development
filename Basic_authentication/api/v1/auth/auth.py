d88d35b28ef61be6bb523ae1688cfbdd3768bc8d
umumararungu <umcynth@gmail.com>
2025-01-22 13:30:42 +0200

    created

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -6,7 +6,14 @@ from flask import request
 from typing import List,TypeVar
 
 
-# class auth():
-#     '''first auth class'''
-#     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        
+class auth:
+    '''first auth class'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''require auth function'''
+        return False
+    def authorization_header(self, request=None) -> str:
+        '''authorization header function'''
+        return None
+    def current_user(self, request=None) -> TypeVar('User'):
+        '''current user fucntion'''
+        return None
