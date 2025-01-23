f25812a560d9f003dbe89d30a835d09c558bd8d0
umumararungu <umcynth@gmail.com>
2025-01-23 14:19:34 +0200

    if

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -17,10 +17,10 @@ class BasicAuth(Auth):
         if authorization_header is None:
             return None
 
-        elif authorization_header is not str:
+        if authorization_header is not str:
             return None
 
-        elif not authorization_header.startwith("Basic "):
+        if not authorization_header.startwith("Basic "):
             return None
 
         return authorization_header[6:]
