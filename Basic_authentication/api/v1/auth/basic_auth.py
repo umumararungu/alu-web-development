2b36cdaf687859a2889ccd0a0143d32c4839ab16
umumararungu <umcynth@gmail.com>
2025-01-23 14:40:31 +0200

    based

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,7 +26,7 @@ class BasicAuth(Auth):
         return authorization_header[6:]
 
     def decode_base64_authorization_header(self,
-     base64_authorization_header: str) -> str:
+                                           base64_authorization_header: str) -> str:
         '''other function'''
         if base64_authorization_header is None:
             return None
