061f53140d7ca906a31258ba31c65b0990c024f5
umumararungu <umcynth@gmail.com>
2025-01-24 12:57:52 +0200

    okokok

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -45,7 +45,7 @@ class BasicAuth(Auth):
         return decoded_value
 
     def extract_user_credentials(self,
-     decoded_base64_authorization_header: str) -> (str, str):
+        decoded_base64_authorization_header: str) -> (str, str):
         """extract function
         """
         if decoded_base64_authorization_header is None:
