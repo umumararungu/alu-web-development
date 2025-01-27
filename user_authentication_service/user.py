ccfad7b579bc1c7f915ef7c99ba3ef1dc31fda85
umumararungu <umcynth@gmail.com>
2025-01-27 17:42:57 +0200

    saved

---
+++ user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""User Authentication"""
+
+import sqlalchemy
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///:memory:', echo=True)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+engine = create_engine('sqlite:///:memory:', echo=True)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    hashed_password = Column(String, nullable=False)
+    reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+       return "<User(email='%s', session_id='%s', hashed_password='%s')>" % (
+                            self.email, self.hashed_password, self.session_id)
