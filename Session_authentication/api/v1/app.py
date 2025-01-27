8667cdd597b31e3f69f24ba0b97c6ae8a188f01c
umumararungu <umcynth@gmail.com>
2025-01-27 17:19:48 +0200

    import

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -5,8 +5,8 @@ Route module for the API
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
-from api.v1.auth.session_db_auth import SessionDBAuth
+# from api.v1.auth.session_exp_auth import SessionExpAuth
+# from api.v1.auth.session_db_auth import SessionDBAuth
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
