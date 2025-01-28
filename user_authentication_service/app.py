aebdf8eb5776307ef849e9e6ae8d87c9a668cbae
umumararungu <umcynth@gmail.com>
2025-01-28 12:36:29 +0200

    new

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -5,10 +5,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
     payload = {"message": "Bienvenue"}
     return jsonify(payload)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
