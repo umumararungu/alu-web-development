2c53642079314e0429c3bcaf04b8b7cfcb354cbd
umumararungu <umcynth@gmail.com>
2025-01-28 12:28:02 +0200

    exist

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -18,7 +18,7 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
-    
+
     def register_user(self, email: str, password: str) -> User:
         try:
             _user = self._db.find_user_by(email=email)
@@ -28,6 +28,3 @@ class Auth:
             return _user
         else:
             raise ValueError('User{email} already exists')
-
-
-        
