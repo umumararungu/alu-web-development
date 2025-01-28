07be14b0bd6bd50dc5c6dce4e7113722fd8e47f0
umumararungu <umcynth@gmail.com>
2025-01-28 11:47:20 +0200

    NON

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -47,7 +47,7 @@ class DB:
 
         return finder
 
-    def update_user(self, user_id: int, **kwargs) ->  User:
+    def update_user(self, user_id: int, **kwargs) ->  None:
         id_to_update = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if not hasattr(id_to_update, key):
