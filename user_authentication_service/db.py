b62a563b535ee15816b1c2f5a2b0b36f1b102140
umumararungu <umcynth@gmail.com>
2025-01-28 11:06:20 +0200

    commit

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -24,7 +24,7 @@ class DB:
     def add_user(self, email = str, hashed_password = str) -> User:
         adding = User(email = email, hashed_password = hashed_password)
         self._session.add(adding)
-        self._session.commit(adding)
+        self._session.commit()
         return adding
 
 
