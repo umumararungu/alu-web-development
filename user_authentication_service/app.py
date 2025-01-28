2173f848714bb8787cbeb6c7be83d898c3d9efd6
umumararungu <umcynth@gmail.com>
2025-01-28 12:34:46 +0200

    payload

---
+++ user_authentication_service/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""""Basic flask app"""
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home(payload):
+    return jsonify(payload)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
