3c8c20966ec8ce31031ff8a683055af60d744930
umumararungu <umcynth@gmail.com>
2025-01-28 12:27:41 +0200

    exist

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -11,3 +11,23 @@ from user import User
 
 def _hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+    
+    def register_user(self, email: str, password: str) -> User:
+        try:
+            _user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            _password = _hash_password(password)
+            _user = self._db.add_user(email, _password)
+            return _user
+        else:
+            raise ValueError('User{email} already exists')
+
+
+        
