5b63ea80fc1148b8b8c489199b2c3a9d763b5be7
umumararungu <umcynth@gmail.com>
2025-01-29 15:21:27 +0200

    check check

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -18,9 +18,11 @@ class Auth:
     """
 
     def __init__(self):
+        """initial function"""
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
+        """registering user function"""
         try:
             _user = self._db.find_user_by(email=email)
         except NoResultFound:
@@ -29,3 +31,14 @@ class Auth:
             return _user
         else:
             raise ValueError('User{email} already exists')
+
+    def valid_login(self, email: str, password: str) ->bool:
+        """login validation function"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        else:
+            return bcrypt.checkpw(password=password.encode('utf-8'),
+                                  hashed_password=user.hashed_password)
+    
