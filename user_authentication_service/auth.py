76bf8dcd6ef53a85d4fd0333774a6de602cacd12
umumararungu <umcynth@gmail.com>
2025-01-30 13:41:26 +0200

    generate

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""hashed passorword"""
+""""hashed password"""
 
 
 import bcrypt
@@ -41,3 +41,6 @@ class Auth:
         else:
             return bcrypt.checkpw(password=password.encode('utf-8'),
                                   hashed_password=user.hashed_password)
+    def _generate_uuid() -> str:
+        """ return a string representation of a new UUID """
+        return str(uuid4())
